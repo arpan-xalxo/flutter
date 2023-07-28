99abbe7860f21bceab634499f8aaa7bf63077b4c
Arpan xalxo <arpan22bcs78@iiitkottayam.ac.in>
2023-07-28 12:39:26 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic_application_1/home_page.dart';
+import 'package:flutter_basic_application_1/pages/home_page.dart';
+import 'package:flutter_basic_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
