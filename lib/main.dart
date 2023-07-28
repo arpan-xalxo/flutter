27f13cd66f2da4adea40154f1f2531f89d7fc707
Arpan xalxo <arpan22bcs78@iiitkottayam.ac.in>
2023-07-28 11:39:01 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
